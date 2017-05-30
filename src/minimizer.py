bbbf0ebfeceec2211bff87a16f85df6d5adc00e0
aekkang <akang@caltech.edu>
2017-05-29 22:01:15 -0700

    Fixed write callback method

--- src/minimizer.py
+++ src/minimizer.py
@@ -86,23 +86,16 @@ class Minimizer(object):
         """Callback method that saves the combined image every particular number of iterations.
         """
 
+        # If enough iterations have been run, this method should do nothing.
+        if self.i > self.iters:
+            return
+
         # Log status.
         self.logfile.write("{}, {:g}\n".format(self.i, self.loss))
         self.logfile.flush()
-        
-        # Check if the minimization has finished.
-        if self.i >= self.iters:
-            # Print status.
-            print("Finished!")
-            print('')
-
-            # Close the log file.
-            self.logfile.close()
-
-            return
 
         # Save the combined image if a sufficient number of iterations has passed.
-        if self.i % self.save_per_n_iters == 0:
+        if self.i % self.save_per_n_iters == 0 or self.i == self.iters:
             # Print status.
             print("Iteration: {}".format(self.i))
             print("Elapsed time: {:.2f}s".format(time.time() - self.start))
@@ -112,5 +105,14 @@ class Minimizer(object):
             # Save the combined image.
             img = np.copy(combination_i)
             imsave(self.combination_prefix + "_{}.jpg".format(self.i), deprocess_img(img, self.width, self.height))
+        
+        # Check if the minimization has finished.
+        if self.i == self.iters:
+            # Print status.
+            print("Finished!")
+            print('')
+
+            # Close the log file.
+            self.logfile.close()
 
         self.i += 1
